0ecdc8f1e64241a3b2def0c512a92a689e5cfe27
ian <ihunter2839@gmail.com>
2020-06-22 08:52:36 -0700

    Created new jinja template for a two column resume format. Added base styling to new resume. Removed .css files from jinjaTemplates folder to avoid duplication and confusion, instead am referencing the css in the parent directory. Isolated core formatting for printing into a shared resume_base.css file

--- web/generateResume.py
+++ web/generateResume.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
 
 	context = json.loads(open(sys.argv[1]).read())
 
-	template = env.get_template('resume.html')
+	template = env.get_template('resume2.html')
 
 	out = open(sys.argv[2], 'w')
 
