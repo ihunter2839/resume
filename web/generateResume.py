9bc39692bf3ff7de677098e27c15e9059fd2fbfb
Ian Hunter <ihunter2839@gmail.com>
2022-09-11 16:40:40 -0700

    Added a new template with some improved graphical elements. Added paths to static files such that css can be stored along side jinja template.

--- web/generateResume.py
+++ web/generateResume.py
@@ -1,20 +1,27 @@
 from jinja2 import Environment, FileSystemLoader
 import sys
 import json
+import os
 
 if __name__ == "__main__":
 
-	fsl = FileSystemLoader('./jinjaTemplates')
+	help_string = "generateResume.py path/to/template path/to/jsonData output.html"
 
-	env = Environment(loader=fsl)
+	if len(sys.argv) < 4 or sys.argv[1] in ["--h", "--help"]:
+		print(help_string)
 
-	context = json.loads(open(sys.argv[1]).read())
+	else:
+		env = Environment( loader=FileSystemLoader(sys.argv[1]) )
 
-	template = env.get_template('resume2.html')
+		context = json.loads(open(sys.argv[2]).read())
 
-	out = open(sys.argv[2], 'w')
+		context["path"] = sys.argv[1]
 
-	out.write(template.render(context))
+		template = env.get_template('template.html')
+
+		out = open(sys.argv[3], 'w')
+
+		out.write(template.render(context))
 
 
 
